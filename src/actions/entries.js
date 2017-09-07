decaporg/decap-cms

Do not close entry if it failed to save or delete.

src/actions/entries.js
@@ -297,7 +297,7 @@ export function persistEntry(collection) {
           kind: 'danger',
           dismissAfter: 8000,
         }));
-        return dispatch(entryPersistFail(collection, serializedEntry, error));
+        return Promise.reject(dispatch(entryPersistFail(collection, serializedEntry, error)));
       });
   };
 }
@@ -319,7 +319,7 @@ export function deleteEntry(collection, slug) {
         dismissAfter: 8000,
       }));
       console.error(error);
-      return dispatch(entryDeleteFail(collection, slug, error));
+      return Promise.reject(dispatch(entryDeleteFail(collection, slug, error)));
     });
   };
 }

LeahHirst/netlify-cms

src/actions/entries.js
@@ -286,7 +286,7 @@ export function persistEntry(collection) {
           kind: 'danger',
           dismissAfter: 8000,
         }));
-        return dispatch(entryPersistFail(collection, entry, error));
+        return Promise.reject(dispatch(entryPersistFail(collection, entry, error)));
       });
   };
 }
@@ -308,7 +308,7 @@ export function deleteEntry(collection, slug) {
         dismissAfter: 8000,
       }));
       console.error(error);
-      return dispatch(entryDeleteFail(collection, slug, error));
+      return Promise.reject(dispatch(entryDeleteFail(collection, slug, error)));
     });
   };
 }